74643159a3ffc5f57904e9a33530cc79c76712d1
Taiyu <222090218@stu.nau.edu.cn>
2023-10-14 21:21:04 +0800

    feat

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,39 @@
 <template>
-  <div>团队功能开发贡献者 / 蒲双畅 / 金太宇</div>
+  <div class="container">
+    <a-card hoverable class="card">
+      <template #cover>
+        <div class="gradient-background">
+          <img src="/favicon.ico" style="width: 200px; margin: 0 auto" />
+        </div>
+      </template>
+      <a-card-meta>
+        <template #description>
+          <div style="text-align: center">
+            <a href="https://github.com/jin-taiyu/RocketTaxi.git">Rocket Taxi</a>
+            <div>团队功能开发贡献者 / 蒲双畅 / 金太宇</div>
+          </div>
+        </template>
+      </a-card-meta>
+    </a-card>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  width: 560px;
+}
+
+.gradient-background {
+  background: linear-gradient(to bottom, #87cfeb72, #f6ff0044);
+  text-align: center;
+  padding: 20px;
+}
+</style>
