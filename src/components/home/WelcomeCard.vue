c820f1f377f9028f988dd5b933bcc9d4da98936f
Taiyu <222090218@stu.nau.edu.cn>
2023-10-14 23:57:29 +0800

    feat(HomePage)：更新了主页

---
+++ src/components/home/WelcomeCard.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const images = ref(['/pic/rt_pic01.jpg', '/pic/rt_pic02.jpg'])
+</script>
+
+<template>
+  <div class="welcome-card">
+    <a-carousel style="width: 100%; height: 100%" :default-current="2" :auto-play="true">
+      <a-carousel-item
+        v-for="(image, key) in images"
+        :key="key"
+        style="display: flex; justify-content: center; align-items: center"
+      >
+        <img :src="image" class="display-image" />
+      </a-carousel-item>
+    </a-carousel>
+
+    <h1 style="color: rgb(44, 166, 247)">欢迎来到Rocket Taxi &nbsp; : )</h1>
+  </div>
+</template>
+
+<style scoped>
+.welcome-card {
+  width: 100%;
+  height: calc(100% - 20px);
+
+  padding: 10px 0;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background-color: white;
+  border-radius: 10px;
+  box-shadow:
+    0 4px 8px 0 rgba(0, 0, 0, 0.2),
+    0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+.display-image {
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,68 @@
 <script setup lang="ts">
+import WelcomeCard from '@/components/home/WelcomeCard.vue'
 </script>
 
 <template>
-  <div>
-    test
+  <div class="home-view">
+    <a-row class="grid" :gutter="20">
+      <a-col :span="12">
+        <div style="flex-direction: column" class="column">
+          <div style="width: 100%; height: 300px; margin-bottom: 10px" class="home-cards">
+            <WelcomeCard />
+          </div>
+          <div style="width: 100%; height: 290px" class="home-cards">
+            <NoticeCard />
+          </div>
+        </div>
+      </a-col>
+
+      <a-col :span="12">
+        <div class="column">
+          <div style="width: 100%; height: 600px" class="home-cards">
+            <UserRankCard />
+          </div>
+        </div>
+      </a-col>
+    </a-row>
+
+    <a-row class="grid" :gutter="20">
+      <a-col :span="12">
+        <div class="column">
+          <div style="width: 100%; height: 400px" class="home-cards">
+            <StatisticsCard />
+          </div>
+        </div>
+      </a-col>
+
+      <a-col :span="12">
+        <div class="column">
+          <div style="width: 100%; height: 400px" class="home-cards">
+            <NewProblemCard />
+          </div>
+        </div>
+      </a-col>
+    </a-row>
   </div>
 </template>
+
+<style scoped>
+.home-view {
+  margin: 5px 5px;
+}
+
+.grid .arco-col {
+  margin-bottom: 10px;
+  text-align: center;
+}
+
+.column {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.home-cards {
+  width: 100%;
+  max-width: 1000px;
+}
+</style>
