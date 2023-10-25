5cb1a25e215d0433acc5240c32556e5c3c933c05
金太宇 <222090218@stu.nau.edu.cn>
2023-10-25 14:56:09 +0800

    feat(打车页面):增加了进度条显示

---
+++ src/components/taxi/ProcessCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="search-card">
+        <div :style="{ display: 'flex' }">
+            <a-card :style="{ width: '100%' }">
+                <a-steps :current="2">
+                    <a-step>等候接单</a-step>
+                    <a-step>司机已接单</a-step>
+                    <a-step>前往目的地</a-step>
+                    <a-step>订单完成</a-step>
+                </a-steps>
+            </a-card>
+        </div>
+    </div>
+</template>
--- src/views/TaxiView.vue
+++ src/views/TaxiView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import MapCard from '@/components/taxi/MapCard.vue'
 import SearchCard from '@/components/taxi/SearchCard.vue'
+import ProcessCard from '@/components/taxi/ProcessCard.vue'
 </script>
 
 <template>
     <div class="taxi-view">
+        <div style="flex-direction: column" class="column">
+            <ProcessCard/>
+        </div>
+
         <div style="flex-direction: column" class="column">
             <MapCard/>
         </div>
